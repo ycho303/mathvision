d1c0d71d7e32f7f2839267b0df6174bde7ca6713
ycho303 <ycho303@gatech.edu>
2020-04-11 00:54:00 +0900

    HW5_Code

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Mathematical Tools for Computer Vision
 
-* Download [OpenCV binary](https://github.com/roricljy/MISC_DATA/releases/download/misc/OpenCV_v3.2.0_32bit.zip) for Windows
+HW_5: RigidBody Rotation
+
+Code Location: RigidBody\ Rotation/Assets/compute_user_input.cs
+
+Used with Unity; attach to a gameObject or open matrix_rotation.unity scene.
---
+++ RigidBody Rotation/Assets/compute_rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compute_rotation : MonoBehaviour {
+	public GameObject source, target;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 source_pos = source.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 target_pos = target.transform.position;
+	}
+}
