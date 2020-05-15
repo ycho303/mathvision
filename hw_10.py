ec6921a5c33006373c60580e439f8dbc7f387256
ycho303 <ycho303@gatech.edu>
2020-05-15 23:03:33 +0900

    added random input
    random input to save time

--- hw_10.py
+++ hw_10.py
@@ -1,25 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
-inputs = []
-while True:
-    ans = input(f'Input point in (x, y) format. Press enter when done:\n')
-    if ans == '':
-        if len(inputs) < 2:
-            print('\nMust have at least 2 points.\n')
-        else:
-            break
-    else:
-        if '(' in ans and ')' in ans:
-            ans = ans[1:-1]
-        try:
-            x, y = ans.split(',')
-            x = float(x)
-            y = float(y)
-            inputs.append((x, y))
-            print(f'\nCurrent inputs: {inputs}\n')
-        except:
-            print('Invalid input.\n')
+# '''User inputs'''
+# inputs = []
+# while True:
+#     ans = input(f'Input point in (x, y) format. Press enter when done:\n')
+#     if ans == '':
+#         if len(inputs) < 2:
+#             print('\nMust have at least 2 points.\n')
+#         else:
+#             break
+#     else:
+#         if '(' in ans and ')' in ans:
+#             ans = ans[1:-1]
+#         try:
+#             x, y = ans.split(',')
+#             x = float(x)
+#             y = float(y)
+#             inputs.append((x, y))
+#             print(f'\nCurrent inputs: {inputs}\n')
+#         except:
+#             print('Invalid input.\n')
+
+'''Random input to save time'''
+def rtuple():
+    return (random.uniform(5, 5.5), random.uniform(1, 10))
+inputs = [rtuple() for i in range(10)]
 
 '''Explicit function: y=ax+b'''
 A = np.array([(x, 1) for x, _ in inputs])
@@ -33,7 +40,7 @@ _, _, V = np.linalg.svd(A, full_matrices=False)
 IP = V.T[:, -1]
 
 '''Plot'''
-x = np.linspace(min(min(inputs)), max(max(inputs)), 2)
+x = np.linspace(0, 10, 10)
 Ey = EP[0]*x + EP[1]
 Iy = (-IP[0]*x - IP[2]) / IP[1]
 plt.plot(x, Ey, '-g', label='y=ax+b')
@@ -42,5 +49,6 @@ plt.scatter([i for i, _ in inputs], [i for _, i in inputs], s=6, color='b')
 plt.title('Estimation')
 plt.legend()
 plt.grid()
+plt.xlim([0, 10])
+plt.ylim([0, 10])
 plt.show()
-
